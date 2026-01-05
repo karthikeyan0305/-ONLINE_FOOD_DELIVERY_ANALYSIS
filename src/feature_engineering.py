eff8b4f163e654658f89cf888c382cc0466ac92f
karthikeyan <138009858+karthikeyan0305@users.noreply.github.com>
2026-01-05 10:47:48 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -3,23 +3,23 @@ import pandas as pd
 
 def feature_engineering(df):
 
-    # 🔹 Convert order_date to datetime
+    #  Convert order_date to datetime
     df['order_date'] = pd.to_datetime(df['order_date'])
 
-    # 🔹 Create day_type correctly (FIXED)
+    #  Create day_type correctly (FIXED)
     df['day_type'] = np.where(
         df['order_date'].dt.weekday >= 5,
         "Weekend",
         "Weekday"
     )
 
-    # 🔹 Convert order_time to datetime
+    # Convert order_time to datetime
     df['order_time'] = pd.to_datetime(df['order_time'])
 
-    # 🔹 Extract hour
+    #  Extract hour
     df['order_hour'] = df['order_time'].dt.hour
 
-    # 🔹 Peak / Non-Peak
+    #  Peak / Non-Peak
     df['peak_hour'] = np.where(
         df['order_hour'].between(12, 14) |
         df['order_hour'].between(19, 22),
@@ -27,7 +27,7 @@ def feature_engineering(df):
         "Non-Peak"
     )
 
-    # 🔹 Delivery performance
+    #  Delivery performance
     df['delivery_performance'] = np.where(
         df['delivery_time_min'] <= 30,
         "On-Time",
